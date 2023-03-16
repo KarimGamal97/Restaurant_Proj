6573f9f6be914978aa6a256ce15289f00dfc44fd
KarimGamal97 <kariimgamal1997@gmail.com>
2023-03-17 01:57:00 +0200

    Bla bla

--- js/index.js
+++ js/index.js
@@ -1,24 +1,55 @@
 // Manu 3
-var swiper = new Swiper(".home-slider", {
-  spaceBetween: 30,
-  centeredSlides: true,
-  autoplay: {
-    delay: 7500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  loop: true,
+// var swiper = new Swiper(".home-slider", {
+//   spaceBetween: 30,
+//   centeredSlides: true,
+//   autoplay: {
+//     delay: 7500,
+//     disableOnInteraction: false,
+//   },
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//   },
+//   loop: true,
+// });
+
+// let menu = document.queryselector("#menu");
+// let navbar = document.queryselector(".navbar");
+
+// menu.onclick = () => {
+//   menu.classlist.toggle("fa-times");
+//   navbar.classlist.toggle("active");
+// };
+
+// Shopping Cart
+
+$(document).ready(function () {
+  var counts = 0;
+  $("#increase-btn").click(function () {
+    counts += +1;
+    $("#selected").animate(300, function () {
+      $(this).text(counts);
+    });
+  });
+  $("#decrease-btn").click(function () {
+    counts -= 1;
+    $("#selected").animate(300, function () {
+      $(this).text(counts);
+    });
+  });
 });
 
-let menu = document.queryselector("#menu");
-let navbar = document.queryselector(".navbar");
+// Login Page
+$(".message a").click(function () {
+  $("form").animate({ height: "toggle", opacity: "toggle" }, "slow");
+});
 
-menu.onclick = () => {
-  menu.classlist.toggle("fa-times");
-  navbar.classlist.toggle("active");
-};
+let username = document.getElementById("login-name");
+let userpassword = document.getElementById("user-password");
+let loginBtn = document.querySelector("#login-btn");
 
-// Manu 3
+loginBtn.addEventListener("click", function (e) {
+  console.log(username.value);
+  console.log(userpassword.value);
+  e.preventDefault();
+});
